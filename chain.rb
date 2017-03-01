3c1103ac7c2a05a777dfe9fab1e8a2fac5e0fba9
Nat Welch <nat@natwelch.com>
2017-03-01 11:21:25 -0800

    Make things consistent

--- chain.rb
+++ chain.rb
@@ -7,11 +7,13 @@ Bundler.require(:default)
 chain = MarkovPolo::Chain.new
 
 text = File.read("neuromancer.txt")
-words = text.split(" ")
+words = text.split(" ").map {|i| i.downcase.gsub(/[^a-z0-9\s]/i, '') }
 
 words.each_slice(5) do |wr|
   chain.push wr.join(" ")
 end
+
+File.write("dump.json", JSON.pretty_generate(chain.to_h))
 
 puts chain.generate
 puts chain.generate
