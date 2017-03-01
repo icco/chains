540f6102b4eed1ba61e55b7ee1161716db3eb2e4
Nat Welch <nat@natwelch.com>
2017-03-01 11:33:52 -0800

    add in new file support

--- chain.rb
+++ chain.rb
@@ -6,11 +6,13 @@ Bundler.require(:default)
 
 chain = MarkovPolo::Chain.new
 
-text = File.read("neuromancer.txt")
-words = text.split(" ").map {|i| i.downcase.gsub(/[^a-z0-9\s]/i, '') }
+["neuromancer.txt", "chamber_secrets.txt"].each do |fn|
+  text = File.read(fn)
+  words = text.split(" ").map {|i| i.downcase.gsub(/[^a-z0-9\s]/i, '') }
 
-words.each_slice(5) do |wr|
-  chain.push wr.join(" ")
+  words.each_slice(5) do |wr|
+    chain.push wr.join(" ")
+  end
 end
 
 File.write("dump.json", JSON.pretty_generate(chain.to_h))
